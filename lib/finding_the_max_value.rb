91eb0747a30a6e997d024dd18e5a2c7ef8a54476
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-08 08:36:36 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,3 +2,11 @@ def find_max_value(array)
   new_array = array.sort
   new_array[-1]
 end
+
+def find_max_value(array)
+  count = 0
+  while count < array.length do
+    if array
+    count += 1
+  end
+end
